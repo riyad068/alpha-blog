85487dc9171ae20a9069eab24f07598237d4b3dc
Riya Dhopavkar <riyad068@gmail.com>
2021-07-02 18:32:42 +0530

    users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about' 
+  get 'signup',to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
